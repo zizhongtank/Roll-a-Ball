6ec92a4ace857a11e4125af34aab1d64449db4b8
zizhongtank <32141317+zizhongtank@users.noreply.github.com>
2017-10-25 14:39:12 -0400

    roll a ball
    the first project

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SphereController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private int count;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Coins Collected:" + count;
+        winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float horiInput = Input.GetAxis("Horizontal");
+        float vertiInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horiInput, 0, vertiInput);
+
+        rb.AddForce(movement * speed);
+	}
+     void OnTriggerEnter(Collider other)
+    {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Coins Collected:" + count;
+        if (count == 9)
+        {
+            winText.text = "You Win !!!";
+
+        }
+        
+    }
+}
